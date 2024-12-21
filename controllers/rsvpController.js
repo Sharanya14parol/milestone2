8d51795603252c2afd1c94343425118c95f7a883
Sharanya14parol <156458949+Sharanya14parol@users.noreply.github.com>
2024-12-21 14:22:46 +0530

    modified

--- controllers/rsvpController.js
+++ controllers/rsvpController.js
@@ -1,37 +1,3 @@
-// let rsvpQueue = [];
-// const maxAttendees = 3;
-
-// exports.addRSVP = (req, res) => {
-//   const { name, email, preference, attendance } = req.body;
-
-//   if (!name || !email || !preference || !attendance) {
-//     return res.status(400).json({ message: "All fields are required" });
-//   }
-
-//   if (attendance === "yes")
-//     if (rsvpQueue.length >= maxAttendees) {
-//       return res.status(400).json({
-//         message: "Event is at full capacity, unable to accept new RSVPs.",
-//       });
-//     }
-
-//     rsvpQueue.push({ name, email, preference, attendance });
-//     res.status(201).json({ success: true, message: "RSVP added successfully" });
-//     console.log("Updated RSVP Queue:", rsvpQueue);
-//   } else {
-//     res.status(200).json({
-//       success: false,
-//       message: "RSVP not added since attendance is marked as NO .",
-//     });
-//   }
-// };
-
-// exports.getRSVPs = (req, res) => {
-//   res.status(200).json({ success: true, rsvps: rsvpQueue });
-// };
-
-// exports.rsvpQueue = rsvpQueue;
-
 const fs = require("fs");
 
 let rsvpQueue = [];
